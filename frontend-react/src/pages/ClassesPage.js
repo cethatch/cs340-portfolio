8058266011821ce3584934cd43c107a0b50311d9
cethatch <cethatch@gmail.com>
2024-11-06 22:41:36 -0800

    Added create table and edit columns for ClassesPage

--- frontend-react/src/pages/ClassesPage.js
+++ frontend-react/src/pages/ClassesPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaRegEdit } from "react-icons/fa";
 
 function ClassesPage() {
     return (
@@ -16,6 +17,7 @@ function ClassesPage() {
                                 <th>Duration (Minutes)</th>
                                 <th>Registration Cost (USD)</th>
                                 <th>Description</th>
+                                <th>Edit</th>
                             </tr>
                             <tr>
                                 
@@ -24,6 +26,8 @@ function ClassesPage() {
                                 <td>90</td>
                                 <td>$65.00</td>
                                 <td>Learn how to prepare and roll sushi from scratch.</td>
+                                <td class="editButton"><FaRegEdit /></td>
+
                             </tr>
                             
                         </tbody>
@@ -32,8 +36,42 @@ function ClassesPage() {
                     <p><em>Note: Class entries may not be deleted.</em></p>
 
                 </article>
+                
+                <article id="addNewForm">
+                <h3>Add a new class:</h3>
+                <p><span class='req'>* </span> - Required field.</p>
+                    <div class="form-container">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td><label for="name">New class name: </label><span class="req">* </span></td>
+                                    <td><input type="text" height="50" maxlength="100" required></input></td>
+                                </tr>
 
+                                <tr>
+                                    <td><label for="duration">Class duration (minutes): </label></td>
+                                    <td><input type="number" id="duration" name="duration"></input></td>
+                                </tr>
+
+                                <tr>
+                                    <td><label for="cost">Cost (USD): </label></td>
+                                    <td><input type="number" id="cost" name="cost" placeholder="$0.00"></input></td>
+                                </tr>
+
+                                <tr>
+                                    <td><label for="description">Class description: </label></td>
+                                    <td><input type="text" id="description" name="description"></input></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button class="submitButton">Submit</button>
+                    </div>
+                    
+                </article>    
+           
             </section>
+
+
         </main>
         </>
     );
